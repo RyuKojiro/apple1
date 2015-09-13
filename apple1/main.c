73f32be51e8da53223b6fd84f7ca2acfadf50dcf
Daniel Loffgren <daniel@loffgren.org>
2015-09-13 00:09:01 +0000

    Added an xcode project and svn:external for v6502
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/apple1/trunk@2 64f78de7-aa59-e511-a0e8-0002a5492df0

---
+++ apple1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  apple1
+//
+//  Created by Daniel Loffgren on 9/12/15.
+//  Copyright (c) 2015 Daniel Loffgren. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
